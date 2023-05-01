0f41b86df16c549f8f6b91f1d39c9e8f4f9a3f3d
sunchaofei <sunchaofei@meituan.com>
2023-05-01 08:45:13 +0800

    Refactor Greeting

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,10 +9,11 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
-      </button>
+      <h3>{greeting}! Thank you for visiting!
+        <button onClick={() => setGreeting(randomMessage())}>
+            New Greeting
+        </button>
+      </h3>
     </div>
   );
 }
